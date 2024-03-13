9dae352b9050f04f1fdaab926bc672c3ad56a1c2
arcelcapital <kontcheu23@gmail.com>
2024-03-13 17:22:25 +0100

    Adjust the position and size of the gold box.

--- App.js
+++ App.js
@@ -9,33 +9,39 @@ export default function App() {
         flex: 1,
         flexDirection: "row",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        alignContent: "center",
+        flexWrap: "wrap"
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width:100,
-          height:300,
-          alignSelf: "flex-start"
+          width: 100, 
+          height: 100,
         }}
       />
 
       <View
         style={{
           backgroundColor: "gold",
-          width:100,
-          height:200,
+          width: 100,
+          height: 100,
+          left: 20,
+          top: 20,
+          position:"absolute"
         }}
       />
 
       <View
         style={{
           backgroundColor: "tomato",
-          width:100,
-          height:100,
+          width: 100,
+          height: 100,
         }}
       />
+
+      
     </View>
   );
 }
