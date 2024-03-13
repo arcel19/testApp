efa175f7c1ad220b4c75703f7687d0ea7099dfdd
arcelcapital <kontcheu23@gmail.com>
2024-03-13 12:31:03 +0100

    Update App.js: add TouchableNativeFeedback, TouchableHighlight, TouchableOpacity, and TouchableWithoutFeedback components.

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from "react-native";
 
 export default function App() {
-  console.log("App is running");
+  const handleOnPress = () => console.log("text press");
+
   return (
-    <View style={styles.container}>
-      <Text>Arcelcapital hello test go !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handleOnPress}>
+        Arcelcapital hello test goooo !
+      </Text>
+
+      <TouchableNativeFeedback onPress={() => console.log("touch image")}>
+        {/* <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300, 
+            uri: "https://picsum.photos/200/300",
+          }}
+        /> */}
+        <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}>
+          <Text> hello</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
