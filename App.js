35a21e43080842797136efe5ced69baef0c0abea
arcelcapital <kontcheu23@gmail.com>
2024-03-13 17:01:43 +0100

    Changed app orientation from default to portrait.

--- App.js
+++ App.js
@@ -1,43 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  Image,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
-  Platform,
-  Dimensions,
-} from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  console.log(Dimensions.get("screen"));
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: "50%",
-          justifyContent: "center",
-          alignItems: "center"
+          flex: 2,
         }}
-      >
-        <Text > Hello</Text>
-      </View>
-    </SafeAreaView>
+      />
+
+      <View
+        style={{
+          backgroundColor: "gold",
+          flex: 1,
+        }}
+      />
+
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? 20 : 0,
-  },
-});
