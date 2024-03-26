4e23f0de8fb697b3fcfccb2abe5e0a490d8b985c
arcelcapital <kontcheu23@gmail.com>
2024-03-26 08:23:07 +0100

    "Add onPress prop to ListItem, update ListItem to accept onPress, add ListItemSeparator to MessagesScreen, and add light color to colors.js"

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import { FlatList,StyleSheet } from 'react-native';
-import ListItem from '../components/ListItem';
-import Screen from '../components/Screen';
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
+import ListItemSeparator from "../components/ListItemSeparator";
 
 const messages = [
-    { 
-        id:1,
-        title:"t1",
-        image:require('../assets/favicon.png')
-    },
-    { 
-        id:2,
-        title:"t2",
-        image:require('../assets/favicon.png')
-    }
-]
+  {
+    id: 1,
+    title: "t1",
+    image: require("../assets/favicon.png"),
+  },
+  {
+    id: 2,
+    title: "t2",
+    image: require("../assets/favicon.png"),
+  },
+];
 
 function MessagesScreen(props) {
-    return (
-        <Screen>
-            <FlatList 
-            data={messages}
-            keyExtractor={message =>message.id.toString}
-            renderItem={({item})=> 
-            <ListItem
-                title={item.title}
-                subTitle={item.description}
-                image={item.image} /> }
-            />
-        </Screen>
-    );
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log("message selected", item)}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+      />
+    </Screen>
+  );
 }
 
-const styles = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({});
 
 export default MessagesScreen;
